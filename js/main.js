735a2b008ddec10cea4639d05d1f894f10ae008f
Boidin Benoît <benoit.bboidin@gmail.com>
2023-07-12 13:20:18 +0200

    Adding logos and bluring divs.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-async function init() {
+async function init_skills() {
     console.log("init()");
 
     try{
@@ -6,21 +6,22 @@ async function init() {
         const skills = await response.json();
 
         for (const skill of skills) {
+            // Container for each skill.
             const skillDiv = document.createElement("div");
             skillDiv.classList.add("skill-div");
             
+            // Skill elements.
             const skillImg = document.createElement("img");
             skillImg.classList.add("skill-img");
             skillImg.src = skill.img;
-
             const skillTitle = document.createElement("h2");
             skillTitle.classList.add("skill-title");
-            skillTitle.textContent = skill.title;
-            
+            skillTitle.textContent = skill.title;    
             const skillDescription = document.createElement("p");
             skillDescription.classList.add("skill-description");
             skillDescription.textContent = skill.description;
 
+            // Add to skill container.
             const skillContainer = document.querySelector(".skill-container");
             skillContainer.appendChild(skillDiv);
             skillDiv.appendChild(skillImg);
@@ -32,4 +33,4 @@ async function init() {
     }
 }
   
-init();
+init_skills();
