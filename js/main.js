f8af973eabaa9cbca54a3ca166e1286c47cf48b5
Boidin Benoît <benoit.bboidin@gmail.com>
2023-07-15 09:17:55 +0200

    Fixing css tab-bar.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 async function init_skills() {
-    console.log("init()");
-
     try{
         const response = await fetch("../data/skills.json");
         const skills = await response.json();
@@ -27,6 +25,39 @@ async function init_skills() {
             skillDiv.appendChild(skillImg);
             skillDiv.appendChild(skillTitle);
             skillDiv.appendChild(skillDescription);
+        }
+    }catch(error){
+        console.log(error);
+    }
+}
+
+async function init_experiences() {
+    try{
+        const response = await fetch("../data/experiences.json");
+        const experiences = await response.json();
+
+        for (const experience of experiences) {
+            // Container for each experience.
+            const experienceDiv = document.createElement("div");
+            experienceDiv.classList.add("experience-div");
+
+            // Experience elements.
+            const experienceImg = document.createElement("img");
+            experienceImg.classList.add("experience-img");
+            experienceImg.src = experience.img;
+            const experienceTitle = document.createElement("h2");
+            experienceTitle.classList.add("experience-title");
+            experienceTitle.textContent = experience.title;
+            const experienceDescription = document.createElement("p");
+            experienceDescription.classList.add("experience-description");
+            experienceDescription.textContent = experience.description;
+
+            // Add to experience container.
+            const experienceContainer = document.querySelector(".experience-container");
+            experienceContainer.appendChild(experienceDiv);
+            experienceDiv.appendChild(experienceImg);
+            experienceDiv.appendChild(experienceTitle);
+            experienceDiv.appendChild(experienceDescription);
         }
     }catch(error){
         console.log(error);
