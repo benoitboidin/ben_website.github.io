6fa7cfe31d4fccc018764b52e1b0035bd4436e85
Boidin Benoît <benoit.bboidin@gmail.com>
2023-07-31 13:46:11 +0200

    Managing all tabs. TODO: add eventListeners to be able to navigate.

--- js/main.js
+++ js/main.js
@@ -54,7 +54,7 @@ async function init_tab_description(tab) {
         descriptionDiv.appendChild(descriptionTitle);
         descriptionDiv.appendChild(descriptionText);
 
-        const skillContainer = document.querySelector(".skill-container");
+        const skillContainer = document.querySelector(".content-container");
         skillContainer.appendChild(descriptionDiv);
 
     }catch(error){
@@ -62,39 +62,106 @@ async function init_tab_description(tab) {
     }
 }
 
-async function init_skills() {
-    init_tab_description("skills");
+function contentSkills(content) {
+    for (const skill of content) {
+        // Container for each skill.
+        const skillDiv = document.createElement("div");
+        skillDiv.classList.add("skill-div");
+        
+        // Skill elements.
+        const skillImg = document.createElement("img");
+        skillImg.classList.add("skill-img");
+        skillImg.src = skill.img;
+        const skillTitle = document.createElement("h3");
+        skillTitle.classList.add("skill-title");
+        skillTitle.textContent = skill.title;    
+        const skillDescription = document.createElement("p");
+        skillDescription.classList.add("skill-description");
+        skillDescription.textContent = skill.description;
+
+        // Add to skill container.
+        const contentContainer = document.querySelector(".content-container");
+        contentContainer.appendChild(skillDiv);
+        skillDiv.appendChild(skillImg);
+        skillDiv.appendChild(skillTitle);
+        skillDiv.appendChild(skillDescription);
+    }
+}
+
+function contentExperiences(content) {
+    for (const experience of content) {
+        const experienceDate = document.createElement("div");
+        experienceDate.classList.add("experience-div");
+        
+        const descriptionDate = document.createElement("p");
+        descriptionDate.classList.add("experience-date");
+        descriptionDate.textContent = experience.date;
+        const experienceTitle = document.createElement("h3");
+        experienceTitle.classList.add("experience-title");
+        experienceTitle.textContent = experience.title;    
+        const experienceDescription = document.createElement("p");
+        experienceDescription.classList.add("experience-description");
+        experienceDescription.textContent = experience.description;
+
+        const contentContainer = document.querySelector(".content-container");
+        contentContainer.appendChild(experienceDate);
+        experienceDate.appendChild(descriptionDate);
+        experienceDate.appendChild(experienceTitle);
+        experienceDate.appendChild(experienceDescription);
+    }
+}
+
+function contentOthers(content) {
+    for (const others of content) {
+        const othersDiv = document.createElement("div");
+        othersDiv.classList.add("others-div");
+
+        const othersTitle = document.createElement("h3");
+        othersTitle.classList.add("others-title");
+        othersTitle.textContent = others.title;
+        const othersDescription = document.createElement("p");
+        othersDescription.classList.add("others-description");
+        othersDescription.textContent = others.description;
+        const othersLink = document.createElement("a");
+        othersLink.classList.add("others-link");
+        othersLink.textContent = others.link;
+        othersLink.href = others.link;
+
+        const contentContainer = document.querySelector(".content-container");
+        contentContainer.appendChild(othersDiv);
+        othersDiv.appendChild(othersTitle);
+        othersDiv.appendChild(othersDescription);
+        othersDiv.appendChild(othersLink);
+    }
+}
+
+
+async function init(tab) {
+    init_tab_description(tab);
     try{
-        const response = await fetch("../data/skills.json");
-        const skills = await response.json();
+        const response = await fetch("../data/" + tab + ".json");
+        const content = await response.json();
 
-        for (const skill of skills) {
-            // Container for each skill.
-            const skillDiv = document.createElement("div");
-            skillDiv.classList.add("skill-div");
-            
-            // Skill elements.
-            const skillImg = document.createElement("img");
-            skillImg.classList.add("skill-img");
-            skillImg.src = skill.img;
-            const skillTitle = document.createElement("h3");
-            skillTitle.classList.add("skill-title");
-            skillTitle.textContent = skill.title;    
-            const skillDescription = document.createElement("p");
-            skillDescription.classList.add("skill-description");
-            skillDescription.textContent = skill.description;
-
-            // Add to skill container.
-            const skillContainer = document.querySelector(".skill-container");
-            skillContainer.appendChild(skillDiv);
-            skillDiv.appendChild(skillImg);
-            skillDiv.appendChild(skillTitle);
-            skillDiv.appendChild(skillDescription);
+        switch(tab){
+            case "skills":
+                contentSkills(content);
+                break;
+            case "experiences":
+                contentExperiences(content);
+                break;
+            case "others":
+                contentOthers(content);
+                break;
+            default:
+                contentSkills(content);
+                break;
         }
+
     }catch(error){
         console.log(error);
     }
 }
 
 // tab_handlers();
-init_skills();
+const tab = "others";
+init(tab);
